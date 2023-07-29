69bf0ec448f441d998b133d771cca1d4e93548d9
scottpratt <prattsc@msu.edu>
2023-07-29 09:46:52 -0400

    yada yada

--- scottrun/eostest.cc
+++ scottrun/eostest.cc
@@ -26,10 +26,6 @@ int main(){
 	Csampler *sampler=new Csampler(T0,0.093,parmap,reslist,randy);
 	IntHadronGas->hgasinfo->sampler=sampler;
 	
-	printf("check a\n");
-	//ChIntInfo_Scott *int_scott=new ChIntInfo_Scott(parmap); // note this does (must) not depend on T
-	//int_scott->CalcQuantities(T0,rhoB,rhoQ,rhoS);
-	
 	IntHadronGas->CalcQuantitiesVsEpsilon(epsilon,rhoB,rhoQ,rhoS);
 	
 	
