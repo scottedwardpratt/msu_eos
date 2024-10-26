a8ec05214266a4dcb0996195d7c0ade487ac908a
scottpratt <prattsc@msu.edu>
2024-10-25 20:50:05 -0400

    yada yada

--- software/src/eos.cc
+++ software/src/eos.cc
@@ -4,7 +4,7 @@
 #include "msu_eos/resonances.h"
 //using namespace MSU_EOS;
 
-bool MSU_EOS::USE_POLE_MAXX=false;
+bool MSU_EOS::USE_POLE_MASS=false;
 bool MSU_EOS::MIN_WIDTH=0.001;
 
 using namespace NMSUPratt;
